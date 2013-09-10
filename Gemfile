264a86f595a046be149eefcd777d1fe6f69e866d
Gordon <gordon.robertson@redsoftware.co.uk>
2013-09-10 13:06:57 +0100

    Rolling back the pony code, doesn't work on heroku

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ ruby "2.0.0"
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'newrelic_rpm'
-gem 'pony'
--- web.rb
+++ web.rb
@@ -1,14 +1,5 @@
 require 'sinatra'
 require 'newrelic_rpm'
-require 'pony'
-
-post '/message' do
-    Pony.mail :to => 'gordon.robertson@redsoftware.co.uk',
-              :from => 'enquiry@redsoftware.co.uk',
-              :subject => 'The Subject',
-              :body => 'Hello World'
-end
-    
 
 get '/*' do
   redirect '/index.html'
