21adc4aa9171e7d05f0155a688c42bf2d7438946
Gordon <gordon.robertson@redsoftware.co.uk>
2013-09-10 13:03:02 +0100

    Start of adding mail. Need to test on the remote server

--- web.rb
+++ web.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 require 'newrelic_rpm'
+require 'pony'
+
+post '/message' do
+    Pony.mail :to => 'gordon.robertson@redsoftware.co.uk',
+              :from => 'enquiry@redsoftware.co.uk',
+              :subject => 'The Subject',
+              :body => 'Hello World'
+end
+    
 
 get '/*' do
   redirect '/index.html'
