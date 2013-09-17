e7b12828974c3f434504a9e7de42fad3e8140e5c
Gordon <gordon.robertson@redsoftware.co.uk>
2013-09-17 16:41:23 +0100

    Implementing the email form via postmark

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ ruby "2.0.0"
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'newrelic_rpm'
+gem 'mail'
+gem 'postmark'
--- web.rb
+++ web.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
 require 'newrelic_rpm'
+require 'mail'
+require 'postmark'
+
+post '/message' do
+
+    paramstr = params.to_s
+
+    message = Mail.new do
+      from            'homepage@redsoftware.co.uk'
+      to              'Gordon Robertson <gordon.robertson@redsoftware.co.uk >'
+      subject         'Get In Touch'
+      body            paramstr
+
+      delivery_method Mail::Postmark, :api_key => ENV['POSTMARK_API_KEY']
+    end
+
+    message.deliver
+
+end
 
 get '/*' do
   redirect '/index.html'
